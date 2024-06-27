9e5ed8a605092d5acb6d1d92a911c2f0200803ab
DeepeshNST <deepesh.agrawal@newtonschool.co>
2024-06-27 15:19:09 +0530

    first commit

--- __tests__/test.js
+++ __tests__/test.js
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 
 describe('Usernames API', () => {
     before(async () => {
-        await prisma.username.deleteMany(); // Clear the table before running tests
+        await prisma.username.deleteMany(); 
     });
 
     after(async () => {
@@ -15,29 +15,35 @@ describe('Usernames API', () => {
     });
 
     describe('POST /usernames', () => {
-        it('should create a new username', (done) => {
-            chai.request(app)
+        it('should create a new username', async () => {
+            const res = await chai.request(app)
                 .post('/usernames')
-                .send({ username: 'test_user' })
-                .end((err, res) => {
-                    expect(res).to.have.status(201);
-                    expect(res.body).to.be.an('object');
-                    expect(res.body).to.have.property('username', 'test_user');
-                    done();
-                });
+                .send({ username: 'test_user' });
+
+            expect(res).to.have.status(201);
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.have.property('username', 'test_user');
+
+            const user = await prisma.username.findFirst({
+                where: { username: 'test_user' }
+            });
+
+            expect(user).to.not.be.null;
+            expect(user).to.have.property('username', 'test_user');
         });
     });
 
     describe('GET /usernames', () => {
-        it('should get all usernames', (done) => {
-            chai.request(app)
-                .get('/usernames')
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body.length).to.be.above(0);
-                    done();
-                });
+        it('should get all usernames', async () => {
+            const res = await chai.request(app).get('/usernames');
+            
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            expect(res.body.length).to.be.above(0);
+
+            const users = await prisma.username.findMany();
+            expect(users).to.be.an('array');
+            expect(users.length).to.be.above(0);
         });
     });
 
@@ -48,21 +54,18 @@ describe('Usernames API', () => {
             userId = user.id;
         });
 
-        it('should update a username by id', (done) => {
-            chai.request(app)
+        it('should update a username by id', async () => {
+            const res = await chai.request(app)
                 .put(`/usernames/${userId}`)
-                .send({ username: 'updated_user' })
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    chai.request(app)
-                        .get('/usernames')
-                        .end((err, res) => {
-                            expect(res).to.have.status(200);
-                            const updatedUser = res.body.find(user => user.id === userId);
-                            expect(updatedUser).to.have.property('username', 'updated_user');
-                            done();
-                        });
-                });
+                .send({ username: 'updated_user' });
+
+            expect(res).to.have.status(200);
+
+            const updatedUser = await prisma.username.findUnique({
+                where: { id: userId }
+            });
+
+            expect(updatedUser).to.have.property('username', 'updated_user');
         });
     });
 
@@ -73,20 +76,16 @@ describe('Usernames API', () => {
             userId = user.id;
         });
 
-        it('should delete a username by id', (done) => {
-            chai.request(app)
-                .delete(`/usernames/${userId}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    chai.request(app)
-                        .get('/usernames')
-                        .end((err, res) => {
-                            expect(res).to.have.status(200);
-                            const deletedUser = res.body.find(user => user.id === userId);
-                            expect(deletedUser).to.be.undefined;
-                            done();
-                        });
-                });
+        it('should delete a username by id', async () => {
+            const res = await chai.request(app).delete(`/usernames/${userId}`);
+            
+            expect(res).to.have.status(200);
+
+            const deletedUser = await prisma.username.findUnique({
+                where: { id: userId }
+            });
+
+            expect(deletedUser).to.be.null;
         });
     });
 });
